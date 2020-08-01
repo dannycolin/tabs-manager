9c4cd61822ba3ea9198c5a1768c646e6453cf0d6
Danny Colin <contact@dannycolin.com>
2020-08-01 18:54:16 -0400

    show tabs count in the browserAction badge

--- src/background.js
+++ src/background.js
@@ -1,10 +1,37 @@
 /**
  * Browser Action
  */
-browser.browserAction.onClicked.addListener(onBrowserActionClicked);
+
+function setBadge() {
+  console.log("hello");
+  browser.browserAction.setBadgeText({
+    text: "1"
+  });
+}
+setBadge();
+
+async function onTabCreated(tab) {
+  let tabs = await browser.tabs.query({});
+
+  browser.browserAction.setBadgeText({
+    text: tabs.length.toString()
+  });
+}
+browser.tabs.onCreated.addListener(onTabCreated);
+
+async function onTabRemoved(tabId, removeInfo) {
+  let tabs = await browser.tabs.query({});
+  let tabsLength = tabs.length - 1;
+
+  browser.browserAction.setBadgeText({
+    text: tabsLength.toString()
+  });
+}
+browser.tabs.onRemoved.addListener(onTabRemoved);
 
 function onBrowserActionClicked() {
   browser.tabs.create({
     url: "/tab-manager.html"
   });
 }
+browser.browserAction.onClicked.addListener(onBrowserActionClicked);
