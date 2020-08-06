97a3c4203fca09786281abfe27cbca5c38628b49
Danny Colin <contact@dannycolin.com>
2020-08-06 19:06:39 -0400

    add tab list, switch and close function

--- src/tab-manager.js
+++ src/tab-manager.js
@@ -5,21 +5,37 @@ async function getTabs() {
 
   for (let tab of tabs) {
     tabItem = document.createElement("li");
-    tabItem.textContent = "Tab " + tab.id;
+    tabItem.className = "tab-item";
+    tabItem.textContent = tab.title;
     tabItem.setAttribute("data-tab-id", tab.id);
+    tabItem.addEventListener("click", onTabClicked, false);
+
+    tabItemCloseButton = document.createElement("button");
+    tabItemCloseButton.textContent = "Close";
+    tabItemCloseButton.addEventListener("click", onTabCloseButtonClicked, false);
+
+    tabItem.appendChild(tabItemCloseButton);
     tabList.appendChild(tabItem);
   }
 }
 
-function updateTabs(action, tabId) {
+function updateTabs(action, tabId, tabTitle) {
   let tabList = document.getElementById("tab-list");
   let tabListChilds = tabList.childNodes;
   let tabItem;
 
   if (action == "add") {
     tabItem = document.createElement("li");
-    tabItem.textContent = "Tab " + tabId;
+    tabItem.className = "tab-item";
+    tabItem.textContent = tabTitle;
     tabItem.setAttribute("data-tab-id", tabId);
+    tabItem.addEventListener("click", onTabClicked, false);
+
+    tabItemCloseButton = document.createElement("button");
+    tabItemCloseButton.textContent = "Close";
+    tabItemCloseButton.addEventListener("click", onTabCloseButtonClicked, false);
+
+    tabItem.appendChild(tabItemCloseButton);
     tabList.appendChild(tabItem);
   } else if (action == "remove") {
     for (let child = 0; child < tabListChilds.length; child++) {
@@ -27,13 +43,14 @@ function updateTabs(action, tabId) {
       let tabListChildId = tabListChild.getAttribute("data-tab-id");
       if (tabListChildId == tabId) {
         tabList.removeChild(tabListChild);
+        tabListChild.removeEventListener("click", onTabClicked, false);
       }
     }
   }
 }
 
 function onTabCreated(tab) {
-  updateTabs("add", tab.id);
+  updateTabs("add", tab.id, tab.title);
 }
 browser.tabs.onCreated.addListener(onTabCreated);
 
@@ -41,6 +58,18 @@ function onTabRemoved(tabId, removeInfo) {
   updateTabs("remove", tabId);
 }
 browser.tabs.onRemoved.addListener(onTabRemoved);
+
+async function onTabClicked(event) {
+  let tab = event.target;
+  let tabId = parseInt(tab.getAttribute("data-tab-id"));
+  await browser.tabs.update(tabId, { active: true });
+}
+
+async function onTabCloseButtonClicked(event) {
+  let tab = event.target.parentNode;
+  let tabId = parseInt(tab.getAttribute("data-tab-id"));
+  await browser.tabs.remove(tabId);
+}
 
 function main() {
   getTabs();
