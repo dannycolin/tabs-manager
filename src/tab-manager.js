59a956ed2c7ebdd779e46959f8b29b4e03005b5b
Danny Colin <contact@dannycolin.com>
2020-08-07 14:43:45 -0400

    update tab title when visiting a new url

--- src/tab-manager.js
+++ src/tab-manager.js
@@ -2,18 +2,23 @@ async function getTabs() {
   let tabs = await browser.tabs.query({});
   let tabList = document.getElementById("tab-list");
   let tabItem;
+  let tabItemTitle;
 
   for (let tab of tabs) {
     tabItem = document.createElement("li");
     tabItem.className = "tab-item";
-    tabItem.textContent = tab.title;
     tabItem.setAttribute("data-tab-id", tab.id);
     tabItem.addEventListener("click", onTabClicked, false);
+
+    tabItemTitle = document.createElement("span");
+    tabItemTitle.className = "tab-title";
+    tabItemTitle.textContent = tab.title;
 
     tabItemCloseButton = document.createElement("button");
     tabItemCloseButton.textContent = "Close";
     tabItemCloseButton.addEventListener("click", onTabCloseButtonClicked, false);
 
+    tabItem.appendChild(tabItemTitle);
     tabItem.appendChild(tabItemCloseButton);
     tabList.appendChild(tabItem);
   }
@@ -23,18 +28,23 @@ function updateTabs(action, tabId, tabTitle) {
   let tabList = document.getElementById("tab-list");
   let tabListChilds = tabList.childNodes;
   let tabItem;
+  let tabItemTitle;
 
   if (action == "add") {
     tabItem = document.createElement("li");
     tabItem.className = "tab-item";
-    tabItem.textContent = tabTitle;
     tabItem.setAttribute("data-tab-id", tabId);
     tabItem.addEventListener("click", onTabClicked, false);
+
+    tabItemTitle = document.createElement("span");
+    tabItemTitle.className = "tab-title";
+    tabItemTitle.textContent = tab.title;
 
     tabItemCloseButton = document.createElement("button");
     tabItemCloseButton.textContent = "Close";
     tabItemCloseButton.addEventListener("click", onTabCloseButtonClicked, false);
 
+    tabItem.appendChild(tabItemTitle);
     tabItem.appendChild(tabItemCloseButton);
     tabList.appendChild(tabItem);
   } else if (action == "remove") {
@@ -44,6 +54,15 @@ function updateTabs(action, tabId, tabTitle) {
       if (tabListChildId == tabId) {
         tabList.removeChild(tabListChild);
         tabListChild.removeEventListener("click", onTabClicked, false);
+      }
+    }
+  } else if (action == "update") {
+    for (let child = 0; child < tabListChilds.length; child++) {
+      let tabListChild = tabListChilds[child];
+      let tabListChildId = tabListChild.getAttribute("data-tab-id");
+      if (tabListChildId == tabId) {
+        let tabListChildTitle = tabListChild.getElementsByClassName("tab-title")[0];
+        tabListChildTitle.textContent = tabTitle;
       }
     }
   }
@@ -58,6 +77,14 @@ function onTabRemoved(tabId, removeInfo) {
   updateTabs("remove", tabId);
 }
 browser.tabs.onRemoved.addListener(onTabRemoved);
+
+async function onTabUpdated(tabId, changeInfo, tabInfo) {
+  if (changeInfo.status == "complete") {
+    let tab = await browser.tabs.get(tabId);
+    updateTabs("update", tabId, tab.title);
+  }
+}
+browser.tabs.onUpdated.addListener(onTabUpdated, {properties: ["status"]});
 
 async function onTabClicked(event) {
   let tab = event.target;
